d6722c8df8c81f5777e76def2b2392204da31963
Paul1234x <paul-hermann.hulla@brg-judenburg.ac.at>
2019-10-14 13:51:30 +0200

    edit first comment

--- app/src/main/java/com/example/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.helloworld2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+    //hallo erster comment
 public class MainActivity extends AppCompatActivity {
 
     @Override
