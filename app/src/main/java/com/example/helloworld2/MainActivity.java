4e79248e82928b788960c4dea8cb5e54a8442347
Paul1234x <paul-hermann.hulla@brg-judenburg.ac.at>
2019-10-21 13:53:25 +0200

    edited four other buttons

--- app/src/main/java/com/example/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.helloworld2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-    //hallo erster comment
+import android.widget.Button;
+
+//hallo erster comment
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,13 +14,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-private button bOne;
+private Button bOne;
 protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     this.bOne = this.findViewById(R.id.bOne);
 
-    this.bone.setOnClickListener(this);
+    this.bOne.setOnClickListener(this);
 
 }
+private Button bTwo;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.bTwo = this.findViewById(R.id.bTwo);
+
+        this.bTwo.setOnClickListener(this);
+
+    }
+    private Button bThree;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.bThree = this.findViewById(R.id.bThree);
+
+        this.bThree.setOnClickListener(this);
+
+    }
+    private Button bFour;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.bFour = this.findViewById(R.id.bFour);
+
+        this.bFour.setOnClickListener(this);
+
+    }
